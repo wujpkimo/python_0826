4af8aec34972717812bc40203d26e9c57b4d9c9b
Jason Wu(Aero 15) <wujpkimo@gmail.com>
2020-09-02 18:58:53 +0800

    Exercist for mouse callback and draw circle

---
+++ demo8_mouse_callback.py
@@ -0,0 +1,34 @@
+import cv2
+import math
+
+center = [0, 0]
+circlefence = [(0, 0)]
+
+def drawCircle(action, x, y, flags, userData):
+    global center
+    global circlefence
+    # print(f"[{action}],[{x}],[{y}]")
+    # print(cv2.EVENT_LBUTTONDOWN, cv2.EVENT_LBUTTONUP)
+    if action == cv2.EVENT_LBUTTONDOWN:
+        center = [(x, y)]
+        cv2.circle(sourceImage, center[0], 1, (255, 255, 0), 6, cv2.LINE_AA)
+    elif action == cv2.EVENT_LBUTTONUP:
+        circlefence = [(x, y)]
+        radius = math.sqrt(math.pow(center[0][0] - circlefence[0][0], 2)+
+                           math.pow(center[0][1] - circlefence[0][1],2))
+        cv2.circle(sourceImage, center[0], int(radius), (0,255, 255),2,cv2.LINE_AA)
+        cv2.imshow("window",sourceImage)
+    pass
+
+
+sourceImage = cv2.imread("./photos/background.jpg", 1)
+
+dummy = sourceImage.copy()
+cv2.namedWindow("window")
+cv2.setMouseCallback("window", drawCircle)
+
+k = 0
+while k != 27:
+    cv2.imshow("window", sourceImage)
+    k = cv2.waitKey(50) & 0xFF
+cv2.destroyAllWindows()
